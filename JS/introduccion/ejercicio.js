f4c23e5c6c679e3637e93fbc9c4aafa614e22afb
Yuliana Samboni <samboniandryyuliana@gmail.com>
2026-02-25 16:44:13 -0500

    first commit

---
+++ JS/introduccion/ejercicio.js
@@ -0,0 +1,8 @@
+alert("LLevar Leche");
+  let huevos = prompt("Hay huevos (si/no)");
+  if(huevos =="si"){
+    alert ("vendame un litro de leche ")
+  }
+  else{
+    alert ("Muchas Gracias")
+  }
---
+++ typescript-intro/src/bases/01_types.ts
@@ -0,0 +1,3 @@
+export const name: string ='Yuliana';
+
+console.log(name);
